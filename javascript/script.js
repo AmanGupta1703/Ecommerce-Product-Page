cdce42587d81fc10a41030f871d7bd75978249b2
Kunal Gupta <89504795+AmanGupta1703@users.noreply.github.com>
2023-11-24 20:55:01 +0530

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,39 +1,30 @@
-// Navigation Elements
-const primaryNavigationEl = document.querySelector(".nav");
-const hamburgerIconEl = document.querySelector(".hamburger-icon");
-const navCloseIconEl = document.querySelector(".nav__close-icon");
-
-// CART
-const cartIconEl = document.querySelector(".cart__icon");
-const cartBoxEl = document.querySelector(".cart-box");
-const cartBoxContentEl = document.querySelector(".cart-box__content");
-const cartCountEl = document.querySelector(".cart__count");
-
-// HERO
-const heroImageEl = document.querySelector(".product-container__hero-img");
-
-// THUMBNAILS
-const thumbnailContainerEl = document.querySelector(".thumbnail-container");
-const thumbnailImgs = document.querySelectorAll(".thumbnail__img");
-
-// QUANTITY SELECTOR
-const quantitySelectorEl = document.querySelector(".quantity-selector");
-const quantitySelectorCountEl = document.querySelector(
-	".quantity-selector__count"
-);
-
-// ADD TO CART
-const btnAddToCartEl = document.querySelector(".btn--add-to-cart");
-
-const btnPrevEl = document.querySelector(".btn--prev");
-const btnNextEl = document.querySelector(".btn--next");
-
-// DATA & VARIABLES
+// Constants
 const PRODUCT_PRICE = 125;
 const PRODUCT_NAME = "Fall Limited Edition Sneakers";
 const THUMBNAIL_BASE_SRC = "images/image-product-1-thumbnail.jpg";
 
-// Constants
+// Selectors
+const selectors = {
+	primaryNavigationEl: document.querySelector(".nav"),
+	hamburgerIconEl: document.querySelector(".hamburger-icon"),
+	navCloseIconEl: document.querySelector(".nav__close-icon"),
+	cartIconEl: document.querySelector(".cart__icon"),
+	cartBoxEl: document.querySelector(".cart-box"),
+	cartBoxContentEl: document.querySelector(".cart-box__content"),
+	cartCountEl: document.querySelector(".cart__count"),
+	heroImageEl: document.querySelector(".product-container__hero-img"),
+	thumbnailContainerEl: document.querySelector(".thumbnail-container"),
+	thumbnailImgs: document.querySelectorAll(".thumbnail__img"),
+	quantitySelectorEl: document.querySelector(".quantity-selector"),
+	quantitySelectorCountEl: document.querySelector(
+		".quantity-selector__count"
+	),
+	btnAddToCartEl: document.querySelector(".btn--add-to-cart"),
+	btnPrevEl: document.querySelector(".btn--prev"),
+	btnNextEl: document.querySelector(".btn--next"),
+};
+
+// Data
 const HERO_IMAGES = [
 	"images/image-product-1.jpg",
 	"images/image-product-2.jpg",
@@ -41,74 +32,58 @@ const HERO_IMAGES = [
 	"images/image-product-4.jpg",
 ];
 
-// Current state
+// State
 let currentImageIndex = 1;
 let currentQuantity = 0;
 
+// Functions
 function getEmptyCartMessage() {
 	return '<h4 class="cart-box__empty-message">Your cart is empty.</h4>';
 }
 
-function renderCartItem(
-	productName = "Fall Limited Edition Sneakers",
-	thumbnailSrc = "images/image-product-1-thumbnail.jpg",
-	unitPrice = PRODUCT_PRICE,
-	quantity = currentQuantity
-) {
+function renderCartItem(productName, thumbnailSrc, unitPrice, quantity) {
 	return `
-      <div class="cart-item cart-item--hide padding-1-5em">
-        <div class="d-flex items-center gap-1 mb-1-5em">
-          <img src="${thumbnailSrc}" alt="Product Thumbnail" class="cart-item__img" />
-          <div class="cart-item__content">
-            <h2 class="cart-item__header mb-5">${productName}</h2>
-            <p class="cart-item__price">
-              <span class="cart-item__unit-price">&dollar;${unitPrice.toFixed(
-					2
-				)}</span>
-              &times;
-              <span class="quantity">${quantity}</span>
-              <span class="cart-item__final-price"><strong>&dollar;${(
-					unitPrice * quantity
-				).toFixed(2)}</strong></span>
-            </p>
-          </div>
-          <button class="btn btn--delete cursor-pointer">
-            <img src="./images/icon-delete.svg" alt="Delete Icon" class="delete-icon" />
-          </button>
+    <div class="cart-item cart-item--hide padding-1-5em">
+      <div class="d-flex items-center gap-1 mb-1-5em">
+        <img src="${thumbnailSrc}" alt="Product Thumbnail" class="cart-item__img" />
+        <div class="cart-item__content">
+          <h2 class="cart-item__header mb-5">${productName}</h2>
+          <p class="cart-item__price">
+            <span class="cart-item__unit-price">&dollar;${unitPrice.toFixed(
+				2
+			)}</span>
+            &times;
+            <span class="quantity">${quantity}</span>
+            <span class="cart-item__final-price"><strong>&dollar;${(
+				unitPrice * quantity
+			).toFixed(2)}</strong></span>
+          </p>
         </div>
-        <button class="btn btn--primary btn--lg">Checkout</button>
+        <button class="btn btn--delete cursor-pointer">
+          <img src="./images/icon-delete.svg" alt="Delete Icon" class="delete-icon" />
+        </button>
       </div>
-    `;
+      <button class="btn btn--primary btn--lg">Checkout</button>
+    </div>
+  `;
 }
 
-function showEmptyCartMessage(currentQuantity) {
+function showEmptyCartMessage() {
 	if (currentQuantity === 0) {
-		cartCountEl.classList.add("cart__count--hide");
-		cartBoxContentEl.innerHTML = "";
-		cartBoxContentEl.insertAdjacentHTML(
-			"afterbegin",
-			getEmptyCartMessage()
-		);
+		selectors.cartCountEl.classList.add("cart__count--hide");
+		selectors.cartBoxContentEl.innerHTML = getEmptyCartMessage();
 	}
 }
-showEmptyCartMessage(currentQuantity);
 
-function showCartItem(
-	productName = "Fall Limited Edition Sneakers",
-	thumbnailSrc = "images/image-product-1-thumbnail.jpg",
-	unitPrice = PRODUCT_PRICE,
-	quantity = currentQuantity
-) {
-	cartBoxContentEl.innerHTML = "";
-	cartBoxContentEl.insertAdjacentHTML(
-		"afterbegin",
-		renderCartItem(productName, thumbnailSrc, unitPrice, quantity)
+function showCartItem() {
+	selectors.cartBoxContentEl.innerHTML = renderCartItem(
+		PRODUCT_NAME,
+		THUMBNAIL_BASE_SRC,
+		PRODUCT_PRICE,
+		currentQuantity
 	);
 }
 
-showEmptyCartMessage(currentQuantity);
-
-// ********************************************** //
 function addZIndexClass(element) {
 	element.classList.add("z-index-2");
 }
@@ -117,57 +92,55 @@ function removeZIndexClass(element) {
 	element.classList.remove("z-index-2");
 }
 
-const toggleNavActive = () => {
-	const isNavActive = primaryNavigationEl.classList.contains("nav--active");
+// Event Handlers
+function toggleNavActive() {
+	const isNavActive =
+		selectors.primaryNavigationEl.classList.contains("nav--active");
 
 	if (isNavActive) {
-		primaryNavigationEl.classList.remove("nav--active");
-		removeZIndexClass(btnNextEl);
-		removeZIndexClass(btnPrevEl);
+		selectors.primaryNavigationEl.classList.remove("nav--active");
+		removeZIndexClass(selectors.btnNextEl);
+		removeZIndexClass(selectors.btnPrevEl);
 	} else {
-		primaryNavigationEl.classList.add("nav--active");
-		addZIndexClass(btnNextEl);
-		addZIndexClass(btnPrevEl);
+		selectors.primaryNavigationEl.classList.add("nav--active");
+		addZIndexClass(selectors.btnNextEl);
+		addZIndexClass(selectors.btnPrevEl);
 	}
-};
+}
 
 function toggleCartBox() {
-	cartBoxEl.classList.toggle("cart-box--hide");
+	selectors.cartBoxEl.classList.toggle("cart-box--hide");
 }
 
 function updateImage(direction) {
-	if (direction === "prev")
-		currentImageIndex =
-			currentImageIndex === 0
+	currentImageIndex =
+		direction === "prev"
+			? currentImageIndex === 0
 				? HERO_IMAGES.length - 1
-				: currentImageIndex - 1;
-	else
-		currentImageIndex =
-			currentImageIndex === HERO_IMAGES.length - 1
-				? 0
-				: currentImageIndex + 1;
-
-	heroImageEl.src = HERO_IMAGES[currentImageIndex];
+				: currentImageIndex - 1
+			: currentImageIndex === HERO_IMAGES.length - 1
+			? 0
+			: currentImageIndex + 1;
+	selectors.heroImageEl.src = HERO_IMAGES[currentImageIndex];
 }
 
 function handleThumbnailContainerClick(e) {
 	const clickedThumbnailImg = e.target.classList.contains("thumbnail__img");
 
-	if (!clickedThumbnailImg) return;
-
-	const thumbnailImg = e.target;
-	handleThumbnailClick(thumbnailImg);
+	if (clickedThumbnailImg) {
+		const thumbnailImg = e.target;
+		handleThumbnailClick(thumbnailImg);
+	}
 }
 
 function handleThumbnailClick(thumbnailImg) {
 	const { src } = thumbnailImg.dataset;
 
-	thumbnailImgs.forEach((img) =>
+	selectors.thumbnailImgs.forEach((img) =>
 		img.classList.remove("thumbnail__img--active")
 	);
 
-	heroImageEl.src = src;
-
+	selectors.heroImageEl.src = src;
 	thumbnailImg.classList.add("thumbnail__img--active");
 }
 
@@ -184,8 +157,9 @@ function handleQuantityButtonClick(e) {
 			currentQuantity === 10 ? currentQuantity : currentQuantity + 1;
 	}
 
-	if (currentQuantity < 1) {
-		return showEmptyCartMessage(currentQuantity);
+	if (currentQuantity === 0) {
+		showEmptyCartMessage();
+		return;
 	}
 
 	if (isMinusBtnEl) {
@@ -193,52 +167,46 @@ function handleQuantityButtonClick(e) {
 			currentQuantity === 0 ? currentQuantity : currentQuantity - 1;
 	}
 
-	showCartItem(
-		PRODUCT_NAME,
-		THUMBNAIL_BASE_SRC,
-		PRODUCT_PRICE,
-		currentQuantity
-	);
-	cartCountEl.classList.remove("cart__count--hide");
-	cartCountEl.textContent = currentQuantity;
-	quantitySelectorCountEl.textContent = currentQuantity;
+	showCartItem();
+	selectors.cartCountEl.classList.remove("cart__count--hide");
+	selectors.cartCountEl.textContent = currentQuantity;
+	selectors.quantitySelectorCountEl.textContent = currentQuantity;
 	document.querySelector(".cart-item").classList.remove("cart-item--hide");
 }
 
-// EVENT LISTENERS
-// Navigation
-hamburgerIconEl.addEventListener("click", toggleNavActive);
-navCloseIconEl.addEventListener("click", toggleNavActive);
+// Event Listeners
+selectors.hamburgerIconEl.addEventListener("click", toggleNavActive);
+selectors.navCloseIconEl.addEventListener("click", toggleNavActive);
 
-// CART
-cartIconEl.addEventListener("click", toggleCartBox);
+selectors.cartIconEl.addEventListener("click", toggleCartBox);
 
-// HERO
-btnPrevEl.addEventListener("click", (e) => {
-	updateImage("prev");
-});
+selectors.btnPrevEl.addEventListener("click", () => updateImage("prev"));
+selectors.btnNextEl.addEventListener("click", () => updateImage("next"));
 
-btnNextEl.addEventListener("click", (e) => {
-	updateImage("next");
-});
+selectors.thumbnailContainerEl.addEventListener(
+	"click",
+	handleThumbnailContainerClick
+);
 
-// THUMBNAILS
-thumbnailContainerEl.addEventListener("click", handleThumbnailContainerClick);
+selectors.quantitySelectorEl.addEventListener(
+	"click",
+	handleQuantityButtonClick
+);
 
-// QUANTITY SELECTOR
-quantitySelectorEl.addEventListener("click", handleQuantityButtonClick);
-
-// ADD TO CART
-btnAddToCartEl.addEventListener("click", (e) => {
-	if (currentQuantity === 0) alert("add an item...");
-
-	showCartItem();
-	currentQuantity > 0 && cartCountEl.classList.remove("cart__count--hide");
-	currentQuantity > 0 && (cartCountEl.textContent = currentQuantity);
-	currentQuantity > 0 &&
+selectors.btnAddToCartEl.addEventListener("click", () => {
+	if (currentQuantity === 0) {
+		alert("Add an item...");
+	} else {
+		showCartItem();
+		selectors.cartCountEl.classList.remove("cart__count--hide");
+		selectors.cartCountEl.textContent = currentQuantity;
 		document
 			.querySelector(".cart-item")
 			.classList.remove("cart-item--hide");
+	}
 });
 
-quantitySelectorCountEl.textContent = currentQuantity;
+selectors.quantitySelectorCountEl.textContent = currentQuantity;
+
+// Initial setup
+showEmptyCartMessage();
